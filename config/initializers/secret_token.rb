e52261fe3c9e2c42ad21fbef4357b4aaf6a3d77f
Todd Mazierski <toddmazierski@gmail.com>
2011-10-01 21:39:02 -0400

    New Rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DrBotsworth::Application.config.secret_token = 'd5acf903df80c45c21a298b9321b45a45ae6b23814522b86f66169ebaecb2461f031ae824a7d0e0fb3471b4d78b28e9e2aedc676f52e07caf4da69058f8feca3'
