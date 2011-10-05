34359f90e1faaa0c594748ed0e995f5c62199ef9
Todd Mazierski <toddmazierski@gmail.com>
2011-10-04 23:09:00 -0400

    Added support for HTTP basic auth.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
+  authentication = Settings.authentication!
+  if authentication.basic_auth
+    http_basic_authenticate_with :name => authentication[:username], :password => authentication[:password]
+  end
   protect_from_forgery
 end
