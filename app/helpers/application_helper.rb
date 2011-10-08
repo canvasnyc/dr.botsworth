78929891e3b2a3e8614520760f58d33e2cf085ea
Todd Mazierski <toddmazierski@gmail.com>
2011-10-08 09:48:17 -0400

    Added buttons with icons to the index pages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def edit_button
+    content_tag(:span, nil, :class => 'edit button')
+  end
+
+  def destroy_button
+    content_tag(:span, nil, :class => 'destroy button')
+  end
+
+  def new_button
+    content_tag(:span, nil, :class => 'new button')
+  end
 end
