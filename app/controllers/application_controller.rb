7d9a7963008d921e50ffe700d34d3aa414b59dbe
Todd Mazierski <toddmazierski@gmail.com>
2011-11-20 18:33:45 -0500

    Removed ugly Hashie syntax.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  authentication = Settings.authentication!
+  authentication = Settings.authentication
   if authentication.basic_auth
     http_basic_authenticate_with :name => authentication[:username], :password => authentication[:password]
   end
