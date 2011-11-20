8b46155e60654fc93eeae60b9c409d101d696a45
Todd Mazierski <toddmazierski@gmail.com>
2011-11-20 18:26:33 -0500

    Added comments and validation.

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,5 +1,22 @@
 class Log
-  def self.read(lines)
-    `tail -n #{lines} #{File.join(Rails.root, '/log/checkup.log')}`
-  end
-end
+
+  class << self
+
+    # Uses tail to read a certain number of lines from the checkup log (the
+    # output from the environments:checkup Rake task).
+    def read(lines)
+      if self.numeric?(lines) and lines.to_i > 0
+        `tail -n #{lines} #{File.join(Rails.root, '/log/checkup.log')}`
+      else
+        "Invalid parameter: #{lines}"
+      end
+    end
+
+  protected
+
+    def numeric?(value)
+      value =~ /^\d+$/
+    end
+
+  end # self
+end # Log
