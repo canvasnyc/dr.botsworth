e285d5690b6c1953e308177ce1e05f8a6b4bb421
Todd Mazierski <toddmazierski@gmail.com>
2011-11-21 11:33:49 -0500

    Created cache! method.

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -1,3 +1,39 @@
 class Chart < ActiveRecord::Base
   belongs_to :environment
+
+  def self.cache!(environment_id, days = 30)
+
+    series = [:unhealthy_checkups_sum,
+              :average_name_lookup_time,
+              :average_start_transfer_time,
+              :average_total_time,
+              :retries_used_sum]
+    starts_at = days.days.ago.at_midnight
+    ends_at = 0.days.ago.at_midnight - 1
+
+    bins = (1..days).collect do |day|
+      starts_at = day.days.ago.at_midnight
+      ends_at = (day - 1).days.ago.at_midnight - 1
+      Checkup.select(
+        'CAST(SUM(CASE WHEN `healthy`= 0 THEN 1 ELSE 0 END) AS UNSIGNED) AS `unhealthy_checkups_sum`,
+        CAST(AVG(`name_lookup_time`) * 1000 AS UNSIGNED) AS `average_name_lookup_time`,
+        CAST(AVG(`start_transfer_time`) * 1000 AS UNSIGNED) AS `average_start_transfer_time`,
+        CAST(AVG(`total_time`) * 1000 AS UNSIGNED) AS `average_total_time`,
+        CAST(SUM(`retries_used`) AS UNSIGNED) AS `retries_used_sum`'
+      ).where(:created_at => starts_at..ends_at, :environment_id => environment_id).first
+    end.reverse
+
+    Chart.where(:environment_id => environment_id).destroy_all
+
+    series.each do |series|
+      data = bins.map { |bin| bin[series] }
+      Chart.create(:environment_id => environment_id,
+                :starts_at => starts_at,
+                :ends_at => ends_at,
+                :series => series,
+                :data => data.to_json)
+    end
+
+  end
+
 end
