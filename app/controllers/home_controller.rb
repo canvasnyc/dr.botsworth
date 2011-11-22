9d210a3ec2198fcde93e1495609c6af015e4f39c
Todd Mazierski <toddmazierski@gmail.com>
2011-11-21 21:44:53 -0500

    Added a site-wide chart to the home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,26 @@
 class HomeController < ApplicationController
   # GET /
   def index
+
+    @charts = []
+
+    Site.all.each do |site|
+      site.environments.each do |environment|
+        chart = environment.charts.where(:series =>
+          'unhealthy_checkups_sum').first
+        @charts << {
+          :name => "#{site.name} #{environment.name}",
+          :data => chart.data
+        }
+      end
+    end
+
+    @starts_at = Chart.first.starts_at
+    @ends_at = Chart.first.ends_at    
+    @point_interval = 1.day * 1000
+    @point_start = @starts_at.to_i * 1000
+    @subtitle = "#{l @starts_at} through #{l @ends_at}"
+
     respond_to do |format|
       format.html # index.html.erb
     end
